9cf17fb31d550ac0dff84313ffca99322f492f8c
ablahum <mriztama@gmail.com>
2023-05-23 23:22:50 +0700

    refacto

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -5,28 +5,28 @@
     <div class="col-xs-12 col-sm-12 col-md-12">
       <div class="card">
         <div class="card-body">
-          <form action="{{ route('student.update', $student->id) }}" method='POST'>
+          <form action="{{ route('user.update', $user->id) }}" method='POST'>
             @csrf
             @method('patch')
             
             <div class="mb-3">
-              <label for="nim" class="form-label">NIM</label>
-              <input type="number" class="form-control" id="nim" value="{{ old('nim') ?? $student->nim }}" name="nim">
+              <label for="nrp" class="form-label">NRP</label>
+              <input type="number" class="form-control" id="nrp" value="{{ old('nrp') ?? $user->nrp }}" name="nrp">
             </div>
             
             <div class="mb-3">
               <label for="name" class="form-label">Name</label>
-              <input type="text" class="form-control" id="name" value="{{ old('name') ?? $student->name }}" name="name">
+              <input type="text" class="form-control" id="name" value="{{ old('name') ?? $user->name }}" name="name">
             </div>
 
             <div class="mb-3">
               <label for="email" class="form-label">Email</label>
-              <input type="email" class="form-control" id="email" value="{{ old('email') ?? $student->email }}" name="email">
+              <input type="email" class="form-control" id="email" value="{{ old('email') ?? $user->email }}" name="email">
             </div>
 
             <div class="mb-3">
               <label for="phone" class="form-label">Phone</label>
-              <input type="number" class="form-control" id="phone" value="{{ old('phone') ?? $student->phone }}" name="phone">
+              <input type="number" class="form-control" id="phone" value="{{ old('phone') ?? $user->phone }}" name="phone">
             </div>
 
             <div class="mb-3">
@@ -34,12 +34,12 @@
 
               <div>
                 <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="gender" id="male" value='Male' {{ $student->gender == 'Male' ? 'checked="checked"' : '' }}>
+                  <input class="form-check-input" type="radio" name="gender" id="male" value='Male' {{ $user->gender == 'Male' ? 'checked="checked"' : '' }}>
                   <label class="form-check-label" for="male">Male</label>
                 </div>
                 
                 <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="gender" id="female" value='Female' {{ $student->gender == 'Female' ? 'checked="checked"' : '' }}>
+                  <input class="form-check-input" type="radio" name="gender" id="female" value='Female' {{ $user->gender == 'Female' ? 'checked="checked"' : '' }}>
                   <label class="form-check-label" for="female">Female</label>
                 </div>
               </div>
@@ -47,7 +47,7 @@
 
             <div class="mb-3">
               <label for="birth_date" class="form-label">Birth Date</label>
-              <input type="date" class="form-control" id="birth_date" value="{{ old('birth_date') ?? $student->birth_date }}" name="birth_date">
+              <input type="date" class="form-control" id="birth_date" value="{{ old('birth_date') ?? $user->birth_date }}" name="birth_date">
             </div>
 
             <button type="submit" class="btn btn-primary">Update</button>
